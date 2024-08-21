bf01e94f400b394cde9812c03c71b2c0dfb2c3ce
Abega1642 <hei.abega.2@gmail.com>
2024-08-21 09:26:31 +0300

    feat: add categoryMapper

--- src/main/java/dev/razafindratelo/mapper/CategoryMapper.java
+++ src/main/java/dev/razafindratelo/mapper/CategoryMapper.java
@@ -1,4 +1,12 @@
 package dev.razafindratelo.mapper;
 
+import dev.razafindratelo.spendings.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CategoryMapper {
+    private Category category;
+    private double amount;
 }
