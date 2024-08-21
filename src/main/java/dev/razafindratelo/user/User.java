55dd93c56a2b7be4aecf58df25982a469d18fe21
Abega1642 <hei.abega.2@gmail.com>
2024-08-21 08:21:41 +0300

    feat: add user package

--- src/main/java/dev/razafindratelo/user/User.java
+++ src/main/java/dev/razafindratelo/user/User.java
@@ -1,4 +1,14 @@
 package dev.razafindratelo.user;
 
+import dev.razafindratelo.spendings.Spending;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class User {
+    private String firstName;
+    private String lastName;
+    private double monthlyBudget;
+    private List<Spending> spendingsList;
 }
