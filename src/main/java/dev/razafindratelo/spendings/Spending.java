6bfdb858d26532c8169f945c52e78bc5c0389efd
Abega1642 <hei.abega.2@gmail.com>
2024-08-21 08:37:17 +0300

    feat: add AllArgsCOnstructor & ToString annotation to Spenging class

--- src/main/java/dev/razafindratelo/spendings/Spending.java
+++ src/main/java/dev/razafindratelo/spendings/Spending.java
@@ -1,10 +1,14 @@
 package dev.razafindratelo.spendings;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@ToString
 public class Spending {
     private String description;
     private double amount;
